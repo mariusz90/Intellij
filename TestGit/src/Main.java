ce1f6964cd758c4227bf623bc892b8fbeb7ef1eb
mariusz90 <mariusz.mza@gmail.com>
2017-02-03 09:43:18 +0100

    V4

--- TestGit/src/Main.java
+++ TestGit/src/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("HELLO 1");
         System.out.println("HELLO 2");
         System.out.println("HELLO 3");
+        System.out.println("HELLO 4");
     }
 }
