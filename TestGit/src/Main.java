fd2bd31324a5f7f92178b305015da7b54760e58c
mariusz90 <mariusz.mza@gmail.com>
2017-02-03 09:38:43 +0100

    V1

---
+++ TestGit/src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by MariuszZ on 2017-02-03.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("HELLO 1");
+        System.out.println("HELLO 2");
+    }
+}
